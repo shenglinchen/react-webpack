82f51cceeb96c515bef65a3e9e3dd667c5e44d66
chenshenglin <chenshenglin@forchange.tech>
2019-08-27 15:15:38 +0800

    feat: :sparkles: vscode 自动补全 webpack 中的快捷路径

--- src/main/app.tsx
+++ src/main/app.tsx
@@ -1,6 +1,6 @@
 import { hot } from "react-hot-loader/root";
 import * as React from "react";
-import Index from "@/modules";
+import Index from "@/modules/index";
 
 class App extends React.Component<any, any> {
   constructor(props: any) {
