7797eff99b2d905bf59ea92cc40c03b144940759
chenshenglin <linshengchen96@gmail.com>
2019-08-29 17:59:57 +0800

    feat: :sparkles: 将执行构建文件分离

---
+++ build/prod-build.js
@@ -0,0 +1,23 @@
+const prodConfig = require("./webpack.prod.config");
+const webpack = require("webpack");
+const chalk = require("chalk");
+
+webpack(prodConfig, (err, stats) => {
+  if (err) throw err;
+
+  process.stdout.write(
+    stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false
+    }) + "\n \n"
+  );
+
+  console.log(
+    chalk.yellow(
+      "构建的文件 index.html 直接打开的话无法获取到数据 \n 请使用 yarn run server 起服务"
+    )
+  );
+});
