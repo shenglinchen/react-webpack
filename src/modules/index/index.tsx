79e6c788131e9070c4a25df48ef87b066ec1e6a3
chenshenglin <linshengchen96@gmail.com>
2019-09-03 20:45:49 +0800

    feat: :sparkles: 添加 react-router

--- src/modules/index/index.tsx
+++ src/modules/index/index.tsx
@@ -3,6 +3,33 @@ import "./index.less";
 import { addTodo, filter } from "@/store/action";
 import { connect } from "react-redux";
 import store from "@/store/store";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+const About = (props: any) => {
+  return <div>About</div>;
+};
+
+const Users = (props: any) => {
+  return <div>Users</div>;
+};
+
+const Carnitas = () => {
+  return <div>Carnitas</div>;
+};
+
+const Tacos = (props: any) => {
+  const { match } = props;
+  console.log(match);
+  return (
+    <div>
+      Tocos
+      <Route path={match.url + "/carnitas"} component={Carnitas} />
+    </div>
+  );
+};
 
 class Index extends React.Component<any, any> {
   public addTodo = () => {
@@ -13,12 +40,35 @@ class Index extends React.Component<any, any> {
   };
   render() {
     return (
-      <div className="index">
-        index111
-        <div styleName="dd" onClick={this.addTodo}>
-          ddd
+      <Router>
+        <div className="index">
+          index111
+          <div styleName="dd" onClick={this.addTodo}>
+            ddd
+          </div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+            <li>
+              <Link to="/tacos/">Tacos</Link>
+            </li>
+            <li>
+              <Link to="/tacos/carnitas/">Tacos</Link>
+            </li>
+          </ul>
         </div>
-      </div>
+        <Route path="/" exact component={Home} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
+        <Route path="/tacos" component={Tacos} />
+      </Router>
     );
   }
 }
